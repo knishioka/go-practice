aa1024260a9c8c9f23569dabf0aaef9cad18ae94
Kenichiro Nishioka <nishioka@3idea.jp>
2020-01-26 23:58:15 +0900

    Add HTML generator

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -19,26 +20,23 @@ func main() {
 	server.ListenAndServe()
 }
 
+func generateHTML(writer http.ResponseWriter, data interface{}, fn ...string) {
+	var files []string
+	for _, file := range fn {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	threads, err := data.Threads()
 	if err == nil {
 		_, err := session(w, r)
-		public_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/public.navbar.html",
-			"templates/index.html",
-		}
-		private_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/private.navabar.html",
-			"templates/index.html",
-		}
-		var templates *template.Template
 		if err != nil {
-			templates = template.Must(template.ParseFiles(private_tmpl_files))
+			generateHTML(writer, threads, "layout", "public.navbar")
 		} else {
-			templates = template.Must(template.ParseFiles(public_tmpl_files))
+			generateHTML(writer, threads, "layout", "private.navbar")
 		}
-		templates.ExecuteTemplate(w, "layout", threads)
 	}
 }
