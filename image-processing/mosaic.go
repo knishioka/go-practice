36f61526c24597b1908187d01b6e93929e8651d3
Kenichiro Nishioka <nishioka@3idea.jp>
2020-02-16 10:57:17 +0900

    Add combine func

--- image-processing/mosaic.go
+++ image-processing/mosaic.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"image"
+	"image/draw"
 	"image/jpeg"
 	"net/http"
 	"os"
@@ -43,10 +44,10 @@ func mosaic(w http.ResponseWriter, r *http.Request) {
 	bounds := original.Bounds()
 	db := cloneTileDB()
 
-	c1 := cut(original, &db, tileSize, bounds.Min.X, bounds,.Min.Y, bounds.Max.X / 2, bounds.Max.Y / 2)
+	c1 := cut(original, &db, tileSize, bounds.Min.X, bounds.Min.Y, bounds.Max.X/2, bounds.Max.Y/2)
 	c2 := cut(original, &db, tileSize, bounds.X/2, bounds.Min.Y, bounds.Max.X, bounds.Max.Y/2)
 	c3 := cut(original, &db, tileSize, bounds.Min.X, bounds.Max.Y/2, bounds.Max.X/2, bounds.Max.Y)
-	c4 := cut(original, &db, tileSize, bounds.Max.X/2, bound.Max.Y/2, bounds.Max.X, bounds.Max.Y)
+	c4 := cut(original, &db, tileSize, bounds.Max.X/2, bounds.Max.Y/2, bounds.Max.X, bounds.Max.Y)
 	c := combine(bounds, c1, c2, c3, c4)
 
 	buf1 := new(bytes.Buffer)
@@ -76,15 +77,55 @@ func cut(original image.Image, db *DB, tileSize, x1, y1, x2, y2 int) <-chan imag
 				nearest := db.nearest(color)
 				file, err := os.Open(nearest)
 				if err == nil {
-					t := resize(img, tileSize)
-					tile := t.SubImage(t.Bounds())
-					tileBounds := image.Rect(x, y, x+tileSize, y+tileSize)
+					img, _, err := image.Decode(file)
+					if err == nil {
+						t := resize(img, tileSize)
+						tile := t.SubImage(t.Bounds())
+						tileBounds := image.Rect(x, y, x+tileSize, y+tileSize)
+					} else {
+						fmt.Println("error:", err)
+					}
 				} else {
-					fmt.Println("error:", err)
+					fmt.Println("error:", nearest)
 				}
-			} else {
-				fmt.Println("error:", nearest)
 			}
 		}
-	}
+	}()
+}
+
+func combine(r image.Rectangle, c1, c2, c3, c4 <-chan image.Image) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		var wg sync.WaitGroup
+		img := image.NewNRGBA(r)
+		copy := func(dst draw.Image, r image.Rectangle, src image.Image, sp image.Point) {
+			draw.Draw(dst, r, src, sp, draw.Src)
+			wg.Done()
+		}
+		wg.Add(4)
+		var s1, s2, s3, s4 image.Image
+		var ok1, ok2, ok3, ok4 bool
+		for {
+			select {
+			case s1, ok1 = <-c1:
+				go copy(img, s1.Bounds(), s1, image.Point{r.Min.X, r.Min.Y})
+			case s2, ok2 = <-c2:
+				go copy(img, s2.Bounds(), s2, image.Point{r.Max.X / 2, r.Min.Y})
+			case s3, ok3 = <-c3:
+				go copy(img, s3.Bounds(), s3, image.Point{r.Min.X, r.Max.Y / 2})
+			case s4, ok4 = <-c4:
+				go copy(img, s4.Bounds(), s4, image.Point{r.Max.X / 2, r.Max.Y / 2})
+			}
+			if ok1 && ok2 && ok3 && ok4 {
+				break
+			}
+
+			wg.Wait()
+			buf2 := new(bytes.Buffer)
+			jpeg.Encode(buf2, img, nil)
+			c <- base64.StdEncoding.EncodeToString(buf2.Bytes())
+		}
+	}()
+	return c
 }
